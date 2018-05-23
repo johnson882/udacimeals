86075e1e800a92a11e31687071c045f47ac91669
Ian Johnson <ianjohnson@Mac-Book-Pro.local>
2018-05-23 01:00:28 -0700

    added state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,69 +1,88 @@
-import{
+import { combineReducers } from 'redux'
+
+import {
   ADD_RECIPE,
-  REMOVE_FROM_CALENDAR
+  REMOVE_FROM_CALENDAR,
 } from '../actions'
 
-const initialCalendarState = {
-  sunday: {
-      breakfast: null,
-      lunch: null,
-      dinner: null,
-    },
-    monday: {
-      breakfast: null,
-      lunch: null,
-      dinner: null,
-    },
-    tuesday: {
-      breakfast: null,
-      lunch: null,
-      dinner: null,
-    },
-    wednesday: {
-      breakfast: null,
-      lunch: null,
-      dinner: null,
-    },
-    thursday: {
-      breakfast: null,
-      lunch: null,
-      dinner: null,
-    },
-    friday: {
-      breakfast: null,
-      lunch: null,
-      dinner: null,
-    },
-    saturday: {
-      breakfast: null,
-      lunch: null,
-      dinner: null,
-  },
+function food (state = {}, action) {
+  switch (action.type) {
+    case ADD_RECIPE :
+      const { recipe } = action
 
-}
-
-function calendar (state = initialCalendarState, action){
-  const {day, recipe, meal} = action
-
-  switch(action.type){
-    case: ADD_RECIPE :
-    return {
-      ...state,
-      [day]: {
-        [meal]: recipe.label
+      return {
+        ...state,
+        [recipe.label]: recipe,
       }
-    }
-    case: REMOVE_FROM_CALENDAR:
-    return {
-      ...state,
-      [day]:{
-        ...state[day], // for each day in state change the certain meal to null, removes meal
-        [meal]: null,
-      }
-    }
-    defualt: return state
-
+    default :
+      return state
   }
 }
 
-export default calendar
+const initialCalendarState = {
+  sunday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null,
+  },
+  monday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null,
+  },
+  tuesday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null,
+  },
+  wednesday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null,
+  },
+  thursday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null,
+  },
+  friday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null,
+  },
+  saturday: {
+    breakfast: null,
+    lunch: null,
+    dinner: null,
+  },
+}
+
+function calendar (state = initialCalendarState, action) {
+  const { day, recipe, meal } = action
+
+  switch (action.type) {
+    case ADD_RECIPE :
+      return {
+        ...state,
+        [day]: {
+          ...state[day],
+          [meal]: recipe.label,
+        }
+      }
+    case REMOVE_FROM_CALENDAR :
+      return {
+        ...state,
+        [day]: {
+          ...state[day],
+          [meal]: null,
+        }
+      }
+    default :
+      return state
+  }
+}
+
+export default combineReducers({
+  food,
+  calendar,
+})

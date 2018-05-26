52b7c4c8fd9fc9ceb6054c9e6a025c345e747362
Ian Johnson <ian@IAN-JOHNSON-MBA-TECH-2017.local>
2018-05-25 18:20:19 -0700

    connect()

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,8 @@
 export const ADD_RECIPE = 'ADD_RECIPE'
 export const REMOVE_FROM_CALENDAR = 'REMOVE_FROM_CALENDAR'
 
-export function adRecipe ({day, recipe, meal}) {
-  return{
+export function addRecipe ({ day, recipe, meal }) {
+  return {
     type: ADD_RECIPE,
     recipe,
     day,
@@ -10,11 +10,10 @@ export function adRecipe ({day, recipe, meal}) {
   }
 }
 
-export function removeFromCalendar ({day, meal}) {
-  return{
+export function removeFromCalendar ({ day, meal }) {
+  return {
     type: REMOVE_FROM_CALENDAR,
     day,
     meal,
   }
-
 }
